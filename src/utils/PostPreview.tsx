6401b6b8734ab85bf8f797dd9f9ab27d23c730aa
Çetin Çetindağ <cetindagcetin@gmail.com>
2024-04-02 18:17:17 +0300

    fix

--- src/utils/PostPreview.tsx
+++ src/utils/PostPreview.tsx
@@ -1,5 +1,6 @@
 
-const PostPreview = ({ title }) => {
+
+const PostPreview = ({title} : {title:string}) => {
   return (
     <div className="min-w-fit max-w-fit max-h-64 min-h-64 p-4 font-roboto-condensed text-white text-lg border border-white rounded-lg">
       <h1 className="text-3xl text-bunker-300 font-semibold">{title}</h1>
